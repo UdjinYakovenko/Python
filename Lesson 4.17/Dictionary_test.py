7add289c673125586e9a048a86d81b5fb722e98d
Udjin <vaselinin@gmail.com>
2020-12-05 16:07:38 +0200

    lesson 4.17

---
+++ Lesson 4.17/Dictionary_test.py
@@ -0,0 +1,16 @@
+car = {
+    'model': 'Ford',
+    'color': 'blue',
+    'Characteristics': ['climat', 'radio', '4x4'],
+    'capabilities': {'speed': '220', 'power': '180'}
+}
+print(car['model'])
+
+comp = {
+    'processor': 'Intel 8700k',
+    'ram': '16 gb',
+    'ssd': '256 gb',
+    'Operation system': ['Windows 10', 'Ubuntu 20.04']
+}
+
+print(comp)
