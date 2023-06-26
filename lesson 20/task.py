7b9216d045c6500675027329e9be55c41880658d
Udjin <vaselinin@gmail.com>
2023-06-26 16:45:50 +0300

    add lessons

---
+++ lesson 20/task.py
@@ -0,0 +1,3 @@
+computer = ("Dell", "Inspiron", 2023, "Intel Core i7", 16)
+brand, model, year, processor, ram = computer
+print(brand, model, year, processor, ram)
---
+++ lesson 21/task.py
@@ -0,0 +1,5 @@
+text = "Hello, world!"
+unique_chars = set(text)
+
+print(unique_chars)
+print(type(unique_chars))
